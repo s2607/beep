dd878a75a64fa82e0a6c764b0bee41990b5224a3
phreaker2600 <swwiley@gmail.com>
2016-02-15 19:48:44 +0000

    remove unescisary stuff, silence aplay

--- beep.c
+++ beep.c
@@ -1,10 +1,8 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
-#include<unistd.h>
 #define R 8000
 double fr;
-
 void dieif(char *m, int r) {
 	if(r){
 		printf("%s\n",m);
@@ -18,17 +16,12 @@ int main(int argc,char **argv) {
 	double l;
 	int i=0;
 	dieif("Usage: beep 4500 0.05",(argc!=3));
-	FILE *fp=popen("aplay -t raw >/dev/null","w");
+	FILE *fp=popen("aplay -q -t raw >/dev/null","w");
 	dieif("crap",!fp);
 	fr=atof(*(argv+1));
 	l=atof(*(argv+2));
-	
-	for(i=0;i<l*R;i++) {
+	for(i=0;i<l*R;i++) 
 		fputc(f(i)+128,fp);
-		printf("%f ",f(i));
-	}
 	dieif("eh",fclose(fp));
 	return 0;
 }
-
-
