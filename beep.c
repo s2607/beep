5e90b49a50a9af0fb0d0454de40cee2a5b2297e8
phreaker2600 <swwiley@gmail.com>
2016-11-20 14:33:09 +0000

    you can specify frequency by note letters now

--- beep.c
+++ beep.c
@@ -10,17 +10,29 @@ void dieif(char *m, int r) {
 		exit(1);
 	}
 }
+int fofkey(int k)
+{
+	k=k+49; //move to middle of keyboard
+	return pow(2,(k-49)/12.)*440;
+}
 float f(int x) {
 		return(sin(x*(fr/R)*2*PI)*100);
 }
 int main(int argc,char **argv) {
 	double l;
 	int i=0;
-	dieif("Usage: beep 440 0.05 \n#This beeps A4 for half a tenth of a second.",(argc!=3));
+	if(argc==1){
+		fr=440;
+		l=0.05;
+	}else{
+		dieif("Usage: beep 440 0.05 \n#This beeps A4 for half a tenth of a second.\nbeep A 0.05\n#exactly the same.",(argc!=3));
+		fr=atof(*(argv+1));
+		if(!fr)
+			fr=fofkey(**(argv+1)-'A');
+		l=atof(*(argv+2));
+	}
 	FILE *fp=popen("aplay -r 8000 -q -t raw >/dev/null","w");
 	dieif("crap",!fp);
-	fr=atof(*(argv+1));
-	l=atof(*(argv+2));
 	for(i=0;i<l*R;i++) 
 		fputc(f(i)+128,fp);
 	dieif("eh",fclose(fp));
